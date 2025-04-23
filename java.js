2b7d0b47d4f0be6839a017c076497c7e35ced44c
ChrisA145 <123036719+ChrisA145@users.noreply.github.com>
2025-04-23 16:20:06 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,12 +1,16 @@
-document.querySelectorAll('nav ul li a').forEach(link =>{
-    link.addEventListener('click', function(event) {
-event.preventDefault();
-const targetId = this.getAttribute('href');
-const targetSection = document.querySelector(targetId)
-
-window.scrollTo({
-    top: targetSection.offsetTop,
-    behavior: 'smooth'
-        })
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('nav ul li a').forEach(link => {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        window.scrollTo({
+          top: targetSection.offsetTop,
+          behavior: 'smooth'
+        });
+      });
     });
-});
+  });
+</script>
+
